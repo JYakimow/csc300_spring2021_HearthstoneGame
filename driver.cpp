38639119a5565f1e3c7888953e22bd0b913480c2
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2021-02-15 23:53:40 -0600

    update

--- driver.cpp
+++ driver.cpp
@@ -3,10 +3,14 @@
 int main(int argc, char** argv)
 {
     Card* KingKrush = new Card("King Krush", 8, 8, 8);
-    Card1* KingKrush = new Card("Elf Lord", 3, 2, 4);
-    Card2* KingKrush = new Card("Captin Crounch", 6, 8, 4);
-    Card3* KingKrush = new Card("Goblin Warrior", 9, 14, 3);
-    Card4* KingKrush = new Card(Sorcerer", 5, 6, 5);
+    Card* ElfLord = new Card("Elf Lord", 3, 2, 4);
+    Card* CaptinCrounch = new Card("Captin Crounch", 6, 8, 4);
+    Card* GoblinWarrior = new Card("Goblin Warrior", 9, 14, 3);
+    Card* Sorcerer = new Card("Sorcerer", 5, 6, 5);
     //(*KingKrush).display();
     KingKrush->display();
+    ElfLord->display();
+    CaptinCrounch->display();
+    GoblinWarrior->display();
+    Sorcerer->display();
 }
